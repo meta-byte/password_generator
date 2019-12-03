520b14a0e73b732ee5fc4007b8e8353fc3e6c945
Garrett Howard <hylander.garrett@gmail.com>
2019-12-02 18:35:48 -0700

    edited DOM elements

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,3 +1,13 @@
+//DOM Elements
+var uppercaseElement = document.getElementById('uppercase');
+var lowercaseElement = document.getElementById('lowercase');
+var numbersElement = document.getElementById('numbers');
+var symbolsElement = document.getElementById('symbols');
+var outputElement = document.getElementById('passwordBox');
+var generateElement = document.getElementById('generateButton');
+var clipboardElement = document.getElementById('clipboardButton');
+
+//Randomizing Functions Defined
 function randomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
@@ -7,15 +17,22 @@ function randomUpper() {
 }
 
 function randomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
 function randomSymbol() {
-    var symbols = '!@#$%^&*(){}[]=<>/'
-    return symbols[Math.floor(Math.random() * symbols.length)];
+    var symbol = '!@#$%^&*(){}[]=<>/'
+    return symbol[Math.floor(Math.random() * symbol.length)];
 }
 
 console.log(randomLower())
 console.log(randomUpper())
 console.log(randomNumber())
 console.log(randomSymbol())
+
+//Generate Button Click Function
+
+
+
+
+//Generate Password Function
