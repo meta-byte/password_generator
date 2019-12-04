a7530be56deff4de759ea990b5175239b81f5a43
Garrett Howard <hylander.garrett@gmail.com>
2019-12-03 18:28:05 -0700

    updated generate password function

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -8,11 +8,11 @@ var generateElement = document.getElementById('generateButton');
 var clipboardElement = document.getElementById('clipboardButton');
 
 //Randomizing Functions Defined
-function randomLower() {
+function randomLowercase() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-function randomUpper() {
+function randomUppercase() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
@@ -21,18 +21,42 @@ function randomNumber() {
 }
 
 function randomSymbol() {
-    var symbol = '!@#$%^&*(){}[]=<>/'
-    return symbol[Math.floor(Math.random() * symbol.length)];
+    var symbols = '!@#$%^&*(){}[]=<>/'
+    return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-console.log(randomLower())
-console.log(randomUpper())
-console.log(randomNumber())
-console.log(randomSymbol())
+// Array for storing functions?
+var randomFunctions = [randomLowercase(), randomUppercase(), randomNumber(), randomSymbol()]
+
+
+//For checking what is checked and what isnt?
+// var settings = {
+//     lower: uppercaseElement.checked,
+//     upper: lowercaseElement.checked,
+//     num: numbersElement.checked,
+//     sym: symbolsElement.checked,
+// }
+
 
 //Generate Button Click Function
-
-
+generateElement.onclick = function () {
+    // outputElement.innerText = newPassword
+}
 
 
 //Generate Password Function
+function generatePassword() {
+    var length = 10
+    var newPassword = ""
+    for (var i = 0; i < length; i++) {
+        newPassword += randomLowercase()
+        newPassword += randomUppercase()
+        newPassword += randomNumber()
+        newPassword += randomSymbol()
+
+        console.log(newPassword)
+    }
+}
+
+generatePassword()
+//Copy to Clipboard
